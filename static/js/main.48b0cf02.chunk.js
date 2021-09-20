5ac4229ae859e973bd7a9395b77d51b2d28ce230
Hilary Nelson <hilarynelson@Hilarys-MacBook-Pro.local>
2021-09-20 13:37:32 -0700

    Updates

---
+++ static/js/main.48b0cf02.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-in-class"]=this["webpackJsonpreact-in-class"]||[]).push([[0],{11:function(n,t,e){"use strict";e.r(t);e(1);var c=e(3),i=e.n(c),o=(e(8),e.p,e(9),e(0));var s=function(n){n&&n instanceof Function&&e.e(3).then(e.bind(null,12)).then((function(t){var e=t.getCLS,c=t.getFID,i=t.getFCP,o=t.getLCP,s=t.getTTFB;e(n),c(n),i(n),o(n),s(n)}))};i.a.render(Object(o.jsx)("div",{children:"People"}),document.getElementById("root")),s()},8:function(n,t,e){},9:function(n,t,e){}},[[11,1,2]]]);
+//# sourceMappingURL=main.48b0cf02.chunk.js.map
